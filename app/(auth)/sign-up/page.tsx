7575443b7cb1b083486d7038b6f072158e0cad10
Priyanshu Paul <priyanshupaul90@gmail.com>
2025-12-24 21:37:52 +0530

    Implementing React Hook Form and Hooks in the form layout and onSubmit button

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 
@@ -9,12 +10,56 @@ const SignUp = () => {
     handleSubmit,
     control,
     formState: {errors , isSubmitting}
-  } = useForm<SignUpFormData>()
+  } = useForm<SignUpFormData>({
+
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      country: 'US',
+      investmentGoals: 'Growth',
+      riskTolerance: 'Medium',
+      preferredIndustry: 'Technology'
+    },
+
+    mode: 'onBlur'
+   
+  })
+
+  const onSubmit = async(data:SignUpFormData) => {
+    try {
+
+      console.log(data)
+      
+    } catch (error) {
+
+      console.log(error)
+      
+    }
+  }
+
+
 
   return (
     <>
 
       <h1 className='form-title'>Sign Up & Personalize</h1>
+
+      <form className='space-y-5' onSubmit={handleSubmit(onSubmit)}>
+
+        {/* INPUTS */}
+
+
+
+        {/* Submit button */}
+        <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+
+          {
+            isSubmitting ? "Creating Account ..." : "Start Your Investment Journey"
+          }
+
+        </Button>
+      </form>
 
     
     </>
